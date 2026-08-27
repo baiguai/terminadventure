908118ad6b3de40b2c6625afd12516f40eda3473
baiguai <baiguai.junk@gmail.com>
2026-08-26 23:22:33 -0700

    Initial Push

---
+++ config.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Central configuration - edit values here, all scripts pick them up
+
+APP_NAME="App"
+
+SOURCES=(
+    "src/main.cpp"
+    "src/utils.cpp"
+)
+
+HEADERS=(
+    "src/main.h"
+    "src/utils.h"
+)
