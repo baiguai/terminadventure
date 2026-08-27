471ba95d482502310283668adb694b7f1b4ef724
baiguai <baiguai.junk@gmail.com>
2026-08-27 13:45:31 -0700

    Currently doesn't build - adding basic layout components.

--- config.sh
+++ config.sh
@@ -6,11 +6,15 @@ APP_NAME="terminadventure"
 
 SOURCES=(
     "src/main.cpp"
+    "src/treeview/treeview.cpp"
+    "src/dm_tools/dm_tool.cpp"
 )
 
 HEADERS=(
     "src/main.hpp"
     "src/app_state/app_state.hpp"
+    "src/treeview/treeview.hpp"
+    "src/dm_tools/dm_tool.hpp"
 )
 
 LIBS=(
