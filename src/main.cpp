b5a4277c082f479b063059e2d384b9802c428f45
baiguai <baiguai.junk@gmail.com>
2026-09-01 00:29:07 -0700

    Fixes and interactive screen.

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
+#include <ftxui/dom/elements.hpp>
+#include <ftxui/component/component.hpp>
+#include <ftxui/component/screen_interactive.hpp>
 
 int main()
 {
-    std::cout << "Hello Dave.\n";
+    using namespace ftxui;
+
+    Element document = vbox({
+        text("Hello Dave.") | bold | center,
+        separator(),
+        hbox({
+            text("Left Panel") | border,
+            vbox({
+                text("Main Content Area") | flex,
+                separator(),
+                text("A Footer") | dim,
+            }) | flex,
+        }) | flex,
+    });
+
+    auto component = Renderer([document] { return document; });
+    auto screen = ScreenInteractive::TerminalOutput();
+    screen.Loop(component);
 
     return 0;
 }
